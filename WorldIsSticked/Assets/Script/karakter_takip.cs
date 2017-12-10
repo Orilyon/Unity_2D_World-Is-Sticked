485cb0a5d5711ae5722e024bfa17abf0aa1f36be
Astecry <onur.gorgu@orilyon.com>
2017-12-10 17:54:05 +0300

    karakter takip kodu tamamlandı .

---
+++ WorldIsSticked/Assets/Script/karakter_takip.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class karakter_takip : MonoBehaviour {
+    public Transform target;
+    void FixedUpdate()
+    {
+        Vector3 new_position = target.position;
+        new_position.z = -10;
+        transform.position = new_position;
+
+    }
+}
